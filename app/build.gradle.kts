98e2a2ccf0d0765b3aef7d3a5f9e4ea6cb6cf22a
sakidpilot <st10198310@vcconnect.edu.za>
2024-09-28 05:13:22 +0200

    workflow debug gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.navigation:navigation-compose:2.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
